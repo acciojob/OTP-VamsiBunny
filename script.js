469d6a49caac877414032a7334d253ea4a2b71e1
Vamsi <130114623+VamsiBunny@users.noreply.github.com>
2026-04-07 13:42:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const code=document.getElementsByClassName('code');
+const codeContainer=document.getElementsByClassName('code-container')
+
+for(let i=0;i<code.length;i++){
+	code[i].addEventListener('input',function(){
+		 this.value = this.value.replace(/[^0-9]/g, "");
+		if(this.value.length===1){
+			if(i<code.length-1){
+				code[i+1].focus();
+			}
+		}
+	})
+
+	code[i].addEventListener('keydown', function(e){
+		if(e.key=='Backspace'){
+			if(this.value.length===''){
+				if(i>0){
+					code[i-1].focus();
+					code[i-1].value="";
+				}
+			}
+			else{
+				this.value==='';
+			}
+		}
+	})
+}
